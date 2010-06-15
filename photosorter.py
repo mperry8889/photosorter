95714b24c5a296e5a154f5de58dc959595ce1bf3
matt@macbook.home <none@none>
2010-06-14 23:39:18 -0400

    Checkpoint: fixed some GUI stuff
    TODO: fix bucket list ordering. Should be in order of sort tree for simplicity. Will figure out tomorrow.

--- photosorter.py
+++ photosorter.py
@@ -16,11 +16,16 @@ class PhotoSorter(object):
       self._current_file_index = None
       self._filelist = None
 
+      self._current_bucket = None
+
       self.image = gtk.Image()
       self.progressbar = gtk.ProgressBar()
+      self.sortLabel = gtk.Label()
+
+      self.buckets = ["1960", "1970", "1980", "1990", "2000"]
 
 
-   def _next_file(self):
+   def next_file(self):
       filter = lambda f: f.upper().endswith(".JPG")
 
       if self._current_file is None:
@@ -39,28 +44,83 @@ class PhotoSorter(object):
       self._current_file_index += 1
       return "files/%s" % self._current_file
 
+   def current_file(self):
+      return "files/%s" % self._current_file
+
+
+   def next_bucket(self):
+      if self._current_bucket is None:
+         self._current_bucket = self.buckets[int(len(self.buckets)/2)]
+      else:
+         bucketno = self.buckets.index(self._current_bucket)-1
+         try:
+            self._current_bucket = self.buckets[bucketno]
+         except IndexError:
+            self._current_bucket = self.buckets[0]
+
+      return self._current_bucket
+
+   def current_bucket(self):
+         return self._current_bucket
+
+   def update_bucket(self):
+      self.sortLabel.set_text("Was this picture taken before or after %s?" % self.next_bucket())
+
 
    def keyboard_command(self, widget, event):
+      
       if chr(event.keyval).upper() == "B":
-         self.image.set_from_file("files/d00002.jpg")
-
+         self.update_bucket()
+      
       if chr(event.keyval).upper() == "N":
-         self.image.set_from_file(self._next_file())
+         self.image.set_from_file(self.next_file())
+
+      if chr(event.keyval) == "1":
+         pass
+
+      if chr(event.keyval) == "2":
+         pass
+
+      if chr(event.keyval).upper() == "D":
+         pass
 
       if chr(event.keyval).upper() == "L":
-         img = PILImage.open("files/d00002.jpg").rotate(90)
-         img.save("files/d00002.jpg")
-         self.image.set_from_file("files/d00002.jpg")
+         current = self.current_file()
+         img = PILImage.open(current).rotate(90)
+         img.save(current)
+         self.image.set_from_file(current)
+
+      if chr(event.keyval).upper() == "H":
+         current = self.current_file()
+         img = PILImage.open(current).transpose(PILImage.FLIP_LEFT_RIGHT)
+         img.save(current)
+         self.image.set_from_file(current)
+
+      if chr(event.keyval).upper() == "S":
+         print widget.get_default_size()
+         print widget.get_size()
+         print widget.maximize_initially
+
+
+      if chr(event.keyval).upper() == "Q":
+         self.quit()
+
+      # resize window if necessary
+      if type(widget) == gtk.Window:
+         print "window"
 
       return True
 
+   def quit(self, widget, event):
+      gtk.main_quit()
 
    def main(self):
       window = gtk.Window(gtk.WINDOW_TOPLEVEL)
       window.set_border_width(10)
-      window.connect("delete_event", gtk.main_quit)
+      window.connect("delete_event", self.quit)
       window.connect("key_press_event", self.keyboard_command)
       window.show()
+      window.maximize()
 
       vbox = gtk.VBox()
       vbox.show()
@@ -69,18 +129,18 @@ class PhotoSorter(object):
       self.progressbar.set_fraction(0.5)
       self.progressbar.set_text("15 of 1003")
       self.progressbar.show()
-      vbox.add(self.progressbar)
+      vbox.pack_start(self.progressbar, False, False, 0)
 
-      self.image.set_from_file(self._next_file())
+      self.image.set_from_file(self.next_file())
       self.image.show()
-      vbox.add(self.image)
+      vbox.pack_start(self.image, False, False, 0)
 
-      sortLabel = gtk.Label()
-      sortLabel.set_text("Was this photo taken before or after 1980?")
-      sortLabel.set_justify(gtk.JUSTIFY_CENTER)
-      sortLabel.modify_font(pango.FontDescription("sans 22"))
-      sortLabel.show()
-      vbox.pack_start(sortLabel, False, False, 0)
+      self.sortLabel = gtk.Label()
+      self.sortLabel.set_justify(gtk.JUSTIFY_CENTER)
+      self.sortLabel.modify_font(pango.FontDescription("sans 22"))
+      self.update_bucket()
+      self.sortLabel.show()
+      vbox.pack_start(self.sortLabel, False, False, 0)
 
       keyLabel = gtk.Label()
       keyLabel.set_text("Press 1 for before, 2 for after")
