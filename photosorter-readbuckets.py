f5356fba91e3296f0900f0fc55ec2672cbc34da8
matt@macbook.home <none@none>
2010-07-03 14:09:19 -0400

    Complete simiple readbuckets script to dump a ton of data that can later be used in silly shell commands

--- photosorter-readbuckets.py
+++ photosorter-readbuckets.py
@@ -5,6 +5,10 @@ from photosorter import Bucket
 from photosorter import Photo
 
 if __name__ == "__main__":
-   p = PhotoSorter()
-   print p.buckets
+    p = PhotoSorter()
+
+    for bucket in p.buckets:
+        for state in ["during", "after", "before", "unknown", "unsorted"]:
+            for photo in getattr(bucket, state):
+                print "%s %s %s %s %s" % (state, bucket.year, photo.filename, photo.rotation, photo.flip_horizontal)
 
